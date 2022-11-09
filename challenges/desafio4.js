28d9cc21b6927b9ad9b2e6ba6a4bc766452df9fa
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-09 19:36:36 -0300

    fix: Lint

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,12 +1,12 @@
 db.produtos.find(
   {
-    vendidos: { $gte: 50, $lte: 100 }
+    vendidos: { $gte: 50, $lte: 100 },
   },
   { 
-    nome: 1, vendidos: 1, _id: 0
-  }
+    nome: 1, vendidos: 1, _id: 0,
+  },
 ).sort(
   { 
-    vendidos: 1 
-  }
+    vendidos: 1 ,
+  },
 );
