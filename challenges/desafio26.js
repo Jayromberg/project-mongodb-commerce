d3bead94d7d47aa873a8c098721b821c06c5538d
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-13 13:00:08 -0300

    feat: Desafio 26

---
+++ challenges/desafio26.js
@@ -0,0 +1,23 @@
+db.produtos.updateMany(
+  { valoresNutricionais: { 
+    $elemMatch: { 
+      tipo: "sódio",  
+      percentual: { $gt: 20, $lt: 40 },
+    },
+  }, 
+  },
+  {
+    $push: {
+      tags: {
+        $each: ["contém sódio"],
+      },
+    },
+  },
+);
+
+db.produtos.find(
+  {},
+  { 
+    nome: 1, tags: 1, _id: 0,
+  },
+);
